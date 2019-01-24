5e9b246ff941b4a3ec3ae98ecd903a9c9110e48d
lidongyang <15650785172@163.com>
2019-01-24 16:38:59 +0800

    js3

--- main.js
+++ main.js
@@ -44,6 +44,11 @@ setTimeout(function(){
     siteWelcome.classList.remove('active')
 }, 1000);
 
+let specialTags =  document.querySelectorAll('[data-x]')
+for(let i =0;i<specialTags.length;i++){
+    specialTags[i].classList.add('offset')
+}
+setTimeout(function(){yyy()},1500);
 
 window.onscroll = function(){
     if(window.scrollY > 0){
@@ -51,4 +56,30 @@ window.onscroll = function(){
     }else{
         topNavBar.classList.remove('sticky')
     }
-}
+    yyy()
+}   
+    
+       
+    function yyy(){
+        let specialTags =  document.querySelectorAll('[data-x]')
+        let minIndex = 0
+        for(let i=1;i<specialTags.length;i++){
+            if(Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY)){
+                minIndex = i 
+            }
+        }
+        specialTags[minIndex].classList.remove('offset')
+        let id = specialTags[minIndex].id
+        let a =document.querySelector('a[href="#' + id + '"]')
+        let li = a.parentNode
+        let brothersAndMe = li.parentNode.children
+        for(let i=0;i<brothersAndMe.length;i++){
+            brothersAndMe[i].classList.remove('highlight')
+        }
+        li.classList.add('highlight')
+    
+    }
+    
+   
+
+
