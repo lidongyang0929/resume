a24846e00cac56f6585a6cc4949d2628caf7b4e6
lidongyang <15650785172@163.com>
2019-03-07 12:24:37 +0800

    加数据库

--- js/message.js
+++ js/message.js
@@ -1,28 +1,47 @@
-var APP_ID = 'w9eXin1A4YxK24Nx3bhLeEYA-gzGzoHsz';
-var APP_KEY = 'HoJmutKwll4OOQMxc9lI9mNi';
-
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
-
-
-message.addEventListener('submit',function(e){
-     e.preventDefault()
-     let content = message.querySelector('input[name=content]').value
-     let name = message.querySelector('input[name=name').value
-     var TestObject = AV.Object.extend('message');
-     var testObject = new TestObject();
-     testObject.save({
-     name: name,
-     content: content
-     }).then(function(object) {
-        let li = document.createElement('li')
-        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
-        messageList.appendChild(li)
-     })
-    })
-
+!function(){
+var view = document.querySelector('section.myMessage')
+var controller = {
+  view: null,
+  form:null,
+  init: function(view){
+    this.view=view
+    this.form = document.querySelector('#form')
+    this.initAV()
+    this.loadMessages()
+    this.bindEvents()
+  },
+  initAV: function(){
+    var APP_ID = 'w9eXin1A4YxK24Nx3bhLeEYA-gzGzoHsz';
+    var APP_KEY = 'HoJmutKwll4OOQMxc9lI9mNi';
+    
+    AV.init({
+      appId: APP_ID,
+      appKey: APP_KEY
+    });
+  },
+  bindEvents: function(){
+    this.form.addEventListener(('submit'),(e)=>{
+      e.preventDefault()
+      this.saveMessage()
+     }) 
+  },
+  saveMessage: function(){
+    let content = this.form.querySelector('input[name=content]').value
+    let name = this.form.querySelector('input[name=name').value
+    var TestObject = AV.Object.extend('message');
+    var testObject = new TestObject();
+    testObject.save({
+    name: name,
+    content: content
+    }).then(function(object) {
+     let li = document.createElement('li')
+     li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+     messageList.appendChild(li)
+     this.form.querySelector('input[name=content]').value = ''
+  })
+  },
+  
+   loadMessages: function(){
     var query = new AV.Query('message');
     query.find().then(function (messages) {
      let array = messages.map((item)=>item.attributes)
@@ -30,7 +49,13 @@ message.addEventListener('submit',function(e){
           let li = document.createElement('li')
           li.innerText = `${item.name}: ${item.content}`
           messageList.appendChild(li)
-          message.querySelector('input[name=content]').value = ''
+         
       })
     }, )
+  }
+}
+controller.init(view)
+
   
+
+  }.call()
--- js/topNavBarSticky.js
+++ js/topNavBarSticky.js
@@ -2,25 +2,19 @@
 var view = document.querySelector('#topNavBar')
 var controller = {
     view: null,
-    bindEvents : function(){
-        var view = this.view
-        window.addEventListener('scroll',function(){
-            if (window.scrollY > 0) {
-                this.active
-            } else {
-                this.deactive
-            }}
-        )
-    }.bind(this),
-    active: function(){
-        this.view.classList.add('sticky')
-    },
-    deactive: function(){
-        this.view.classList.remove('sticky')
-    },
     init: function(view){
         this.view = view
         this.bindEvents()
+    },
+    bindEvents : function(){
+        var view = this.view
+        window.addEventListener('scroll',()=>{
+            if (window.scrollY > 0) {
+                this.view.classList.add('sticky')
+            } else {
+                this.view.classList.remove('sticky')
+            }}
+        )
     }
 }
 controller.init(view)
